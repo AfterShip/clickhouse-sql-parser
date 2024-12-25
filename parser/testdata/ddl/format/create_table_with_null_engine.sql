5bb1320d556e401f6478278bb57e4ddf6ba161b3
git-hulk <hulk.website@gmail.com>
2024-12-25 14:50:47 +0800

    Fix should allow to assign parameters for the Null engine

---
+++ parser/testdata/ddl/format/create_table_with_null_engine.sql
@@ -0,0 +1,9 @@
+-- Origin SQL:
+CREATE TABLE logs.t0 on cluster default
+(
+    `trace_id` String CODEC(ZSTD(1)),
+    INDEX trace_id_bloom_idx trace_id TYPE bloom_filter(0.01) GRANULARITY 64
+) ENGINE = Null();
+
+-- Format SQL:
+CREATE TABLE logs.t0 ON CLUSTER default (`trace_id` String CODEC(ZSTD(1)), INDEX trace_id_bloom_idx trace_id TYPE bloom_filter(0.01) GRANULARITY 64) ENGINE = Null();
