0ede5682df8e285fe0cdac2882938ea2546fe0e4
git-hulk <hulk.website@gmail.com>
2025-04-17 20:22:04 +0800

    Fix should allow using the keyword as column name

---
+++ parser/testdata/query/select_with_keyword_in_group_by.sql
@@ -0,0 +1,7 @@
+SELECT
+    toStartOfInterval(timestamp, toIntervalMinute(1)) AS interval,
+    column_name
+FROM table
+WHERE true
+GROUP BY (interval, column_name)
+ORDER BY (interval AS i, column_name) ASC
