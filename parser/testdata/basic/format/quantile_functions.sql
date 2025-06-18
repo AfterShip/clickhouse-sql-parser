508e5bd0df1f4cb4dba70527bc4a8bdc30b3e36b
Keuin <keuinx@gmail.com>
2025-06-18 22:35:29 +0800

    fix: ParamExprList.String is missing ColumnArgList

---
+++ parser/testdata/basic/format/quantile_functions.sql
@@ -0,0 +1,6 @@
+-- Origin SQL:
+SELECT quantile(0.9)(x), quantiles(0.5, 0.9)(x);
+
+
+-- Format SQL:
+SELECT quantile(0.9)(x), quantiles(0.5, 0.9)(x);
---
+++ parser/testdata/basic/quantile_functions.sql
@@ -0,0 +1,1 @@
+SELECT quantile(0.9)(x), quantiles(0.5, 0.9)(x);
