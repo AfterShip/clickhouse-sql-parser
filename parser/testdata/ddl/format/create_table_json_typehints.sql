df1462c124daf3e18eeebdf44a0891ae6332208c
Piyush Singariya <piyushsingariya@gmail.com>
2025-09-29 12:32:36 +0530

    test: run make update_test

--- parser/testdata/ddl/format/create_table_json_typehints.sql
+++ parser/testdata/ddl/format/create_table_json_typehints.sql
@@ -6,7 +6,6 @@ ORDER BY tuple();
 
 
 
+
 -- Format SQL:
 CREATE TABLE t (j JSON(max_dynamic_paths=0, message String, a.b UInt64, SKIP x,  SKIP REGEXP 're')) ENGINE = MergeTree ORDER BY tuple();
-
-
