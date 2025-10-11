a33068af543e073b6981ee1d1934d8dbb7353128
wanghao <wang_hao11@venusgroup.com.cn>
2025-10-11 11:23:59 +0800

    fix: update testdata

--- parser/testdata/ddl/format/create_table_with_sample_by.sql
+++ parser/testdata/ddl/format/create_table_with_sample_by.sql
@@ -12,4 +12,4 @@ SAMPLE BY userid
 SETTINGS index_granularity = 8192;
 
 -- Format SQL:
-CREATE TABLE default.test UUID '87887901-e33c-497e-8788-7901e33c997e' (`f0` DateTime, `f1` UInt32, `f3` UInt32) ENGINE = ReplicatedMergeTree('/clickhouse/tables/{layer}/{shard}/default/test', '{replica}') PARTITION BY toYYYYMM(timestamp) SAMPLE BY userid SETTINGS index_granularity=8192 ORDER BY (contractid, toDate(timestamp), userid);
+CREATE TABLE default.test UUID '87887901-e33c-497e-8788-7901e33c997e' (`f0` DateTime, `f1` UInt32, `f3` UInt32) ENGINE = ReplicatedMergeTree('/clickhouse/tables/{layer}/{shard}/default/test', '{replica}') ORDER BY (contractid, toDate(timestamp), userid) PARTITION BY toYYYYMM(timestamp) SAMPLE BY userid SETTINGS index_granularity=8192;
