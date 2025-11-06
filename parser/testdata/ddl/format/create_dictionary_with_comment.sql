0d35ffe3bc9cc5e17ecb7f63fa8b896560233469
Sharad Gaur <sharadgaur@gmail.com>
2025-11-06 11:00:38 -0500

    Adding support for parsing COMMENT for dictionary
    Signed-off-by: Sharad Gaur <sharadgaur@gmail.com>

---
+++ parser/testdata/ddl/format/create_dictionary_with_comment.sql
@@ -0,0 +1,26 @@
+-- Origin SQL:
+CREATE DICTIONARY test.my_dict (
+    id UInt64,
+    name String DEFAULT '',
+    value Float64 EXPRESSION toFloat64OrZero(name),
+    parent_id UInt64 HIERARCHICAL,
+    is_active UInt8 INJECTIVE,
+    object_id UInt64 IS_OBJECT_ID
+)
+PRIMARY KEY id
+SOURCE(MYSQL(
+    host 'localhost'
+    port 3306
+    user 'default'
+    password ''
+    db 'test'
+    table 'dict_table'
+))
+LIFETIME(MIN 1000 MAX 2000)
+LAYOUT(HASHED())
+SETTINGS(max_block_size = 8192)
+COMMENT 'This is a test dictionary with comment';
+
+
+-- Format SQL:
+CREATE DICTIONARY test.my_dict (id UInt64, name String DEFAULT '', value Float64 EXPRESSION toFloat64OrZero(name), parent_id UInt64 HIERARCHICAL, is_active UInt8 INJECTIVE, object_id UInt64 IS_OBJECT_ID) PRIMARY KEY id SOURCE(MYSQL(host 'localhost' port 3306 user 'default' password '' db 'test' table 'dict_table')) LIFETIME(MIN 1000 MAX 2000) LAYOUT(HASHED()) SETTINGS(max_block_size=8192) COMMENT 'This is a test dictionary with comment';
