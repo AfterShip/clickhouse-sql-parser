b358e1c798165e9f446de79e966b8c41eb0fefe4
Mark deVilliers <markdevilliers@gmail.com>
2025-11-17 14:21:42 +0000

    feat: parse SETTING boolean values

--- parser/testdata/ddl/create_table_with_ttl_policy.sql
+++ parser/testdata/ddl/create_table_with_ttl_policy.sql
@@ -30,4 +30,4 @@ CREATE TABLE table_for_recompression
 ORDER BY tuple()
 PARTITION BY key
 TTL d + INTERVAL 1 MONTH RECOMPRESS CODEC(ZSTD(17)), d + INTERVAL 1 YEAR RECOMPRESS CODEC(LZ4HC(10))
-SETTINGS min_rows_for_wide_part = 0, min_bytes_for_wide_part = 0, foo = true;
+SETTINGS min_rows_for_wide_part = 0, min_bytes_for_wide_part = 0, allow_experimental_replacing_merge_with_cleanup = true;
