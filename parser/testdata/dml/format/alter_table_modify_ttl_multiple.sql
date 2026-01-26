fbea4206789fdefb5f9dbde0afa8cb1e303db6a5
hulk <hulk.website@gmail.com>
2026-01-26 18:14:52 +0800

    Fix parsing failure when having multiple ttl expressions (#234)

---
+++ parser/testdata/dml/format/alter_table_modify_ttl_multiple.sql
@@ -0,0 +1,9 @@
+-- Origin SQL:
+ALTER TABLE db.t0 ON CLUSTER default_cluster
+MODIFY TTL
+    toDateTime(timestamp / 1000000000) + INTERVAL 30 DAY TO DISK 'gcs',
+    toDateTime(timestamp / 1000000000) + INTERVAL 60 DAY;
+
+
+-- Format SQL:
+ALTER TABLE db.t0 ON CLUSTER default_cluster MODIFY TTL toDateTime(timestamp / 1000000000) + INTERVAL 30 DAY TO DISK 'gcs', toDateTime(timestamp / 1000000000) + INTERVAL 60 DAY;
