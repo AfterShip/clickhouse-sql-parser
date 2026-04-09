120ff65369024b9ae49f98094e3c659d5fff05e3
Sharad Gaur <2407900+sharadgaur@users.noreply.github.com>
2026-04-08 22:29:49 -0400

    Support COMMENT before AS SELECT in CREATE MATERIALIZED VIEW (#262)
    ClickHouse 26.2+ changed SHOW CREATE TABLE output to place COMMENT
    before AS SELECT, while 25.x places it after. The parser only
    supported COMMENT after AS SELECT, causing parse failures on 26.2+.
    
    Now tries COMMENT in both positions:
    - Before AS SELECT (ClickHouse 26.2+ SHOW CREATE TABLE format)
    - After AS SELECT (ClickHouse 25.x SHOW CREATE TABLE format)
    
    This closes #261.

--- parser/parser_view.go
+++ parser/parser_view.go
@@ -141,6 +141,15 @@ func (p *Parser) parseCreateMaterializedView(pos Pos) (*CreateMaterializedView, 
 		createMaterializedView.StatementEnd = p.Pos()
 	}
 
+	// COMMENT can appear either before or after AS SELECT.
+	// ClickHouse 26.2+ outputs COMMENT before AS SELECT in SHOW CREATE TABLE,
+	// while 25.8 outputs it after AS SELECT.
+	comment, err := p.tryParseComment()
+	if err != nil {
+		return nil, err
+	}
+	createMaterializedView.Comment = comment
+
 	if p.tryConsumeKeywords(KeywordAs) {
 		subQuery, err := p.parseSubQuery(p.Pos())
 		if err != nil {
@@ -150,11 +159,14 @@ func (p *Parser) parseCreateMaterializedView(pos Pos) (*CreateMaterializedView, 
 		createMaterializedView.StatementEnd = subQuery.End()
 	}
 
-	comment, err := p.tryParseComment()
-	if err != nil {
-		return nil, err
+	// Also try parsing COMMENT after AS SELECT (ClickHouse 25.x format)
+	if createMaterializedView.Comment == nil {
+		comment, err = p.tryParseComment()
+		if err != nil {
+			return nil, err
+		}
+		createMaterializedView.Comment = comment
 	}
-	createMaterializedView.Comment = comment
 	return createMaterializedView, nil
 }
 
